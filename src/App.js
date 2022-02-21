83b4469ec20114ae5b6f8aa98cd8019364591599
RenataNogDias <40239540+RenataNogDias@users.noreply.github.com>
2022-02-21 21:19:22 +0000

    created dictionary component with search box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from "./images/logo.png";
 import "./App.css";
 
+import Dictionary from "./Dictionary";
+
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="she codes logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="she codes logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/RenataNogDias/dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open source code
+            </a>{" "}
+            by Renata Dias
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
