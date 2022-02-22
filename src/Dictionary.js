9179812d1944f65c5ef419978a10b1c6afd8c853
RenataNogDias <40239540+RenataNogDias@users.noreply.github.com>
2022-02-22 22:40:04 +0000

    added the word meanings to the page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
+    setResults(response.data[0]);
+    console.log(response);
     console.log(response.data[0]);
   }
 
@@ -30,6 +35,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
