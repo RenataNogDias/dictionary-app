fa0f38eaefbeffba8d8b79702258b217d84c665f
RenataNogDias <40239540+RenataNogDias@users.noreply.github.com>
2022-02-24 21:06:48 +0000

    added some css

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Phonetic(props) {
-  console.log(props);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
+        <FontAwesomeIcon icon="fa-solid fa-volume-low" />
       </a>
-      <br />
+
       {props.phonetic.text}
     </div>
   );
